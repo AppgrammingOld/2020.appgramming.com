80cb092f9dd5d20fcbca4de35964de25d525d7be
helloworldwriter <writer@helloworldwriter.com>
2020-06-20 11:53:03 +0300

    First commit

---
+++ _eleventy/config.js
@@ -0,0 +1,25 @@
+const yaml = require("js-yaml");
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+  
+  eleventyConfig.addPassthroughCopy('src/assets/css/*.*');
+  eleventyConfig.addPassthroughCopy('src/assets/js/*.*');
+  eleventyConfig.addPassthroughCopy('src/assets/libs/**/*.*');
+  eleventyConfig.addPassthroughCopy('src/CNAME');
+
+  eleventyConfig.setDataDeepMerge(true);
+
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
+    passthroughFileCopy: true,
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  }
+}
---
+++ src/_data/site.js
@@ -0,0 +1,4 @@
+module.exports = {
+  baseImageUrl: 'https://innovatools.b-cdn.net/images',
+  currentYear: new Date().getFullYear()
+};
